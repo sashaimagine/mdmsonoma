e6e398ef8a11a0e83a84f3c7c34f03e4122c597f
sashaimagine <154315869+sashaimagine@users.noreply.github.com>
2025-09-16 10:05:55 +0300

    mdm.sh

--- mdm.sh
+++ mdm.sh
@@ -7,7 +7,7 @@ echo ""
 echo -e "Auto Tools for MacOS"
 echo ""
 PS3='Please enter your choice: '
-options=("Bypass on Recovery" "Disable Notification (SIP)" "Disable Notification (Recovery)" "Check MDM Enrollment" "Thoát")
+options=("Bypass on Recovery" "Disable Notification (SIP)" "Disable Notification (Recovery)" "Check MDM Enrollment" "Quit")
 select opt in "${options[@]}"; do
 	case $opt in
 	"Bypass on Recovery")
@@ -15,19 +15,19 @@ select opt in "${options[@]}"; do
 		if [ -d "/Volumes/OSX - Data" ]; then
    			diskutil rename "OSX - Data" "Data"
 		fi
-		echo -e "${GRN}Tạo người dùng mới"
-        echo -e "${BLU}Nhấn Enter để chuyển bước tiếp theo, có thể không điền sẽ tự động nhận giá trị mặc định"
-  		echo -e "Nhập tên người dùng (Mặc định: MAC)"
+		echo -e "${GRN}Create new user"
+        echo -e "${BLU}Press Enter to proceed to next step, leaving blank will automatically use default value"
+  		echo -e "Enter user name (Default: MAC)"
 		read realName
   		realName="${realName:=MAC}"
-    	echo -e "${BLUE}Nhận username ${RED}VIẾT LIỀN KHÔNG DẤU ${GRN} (Mặc định: MAC)"
+    	echo -e "${BLUE}Enter username ${RED}WRITE WITHOUT SPACES OR ACCENTS ${GRN} (Default: MAC)"
       	read username
 		username="${username:=MAC}"
-  		echo -e "${BLUE}Nhập mật khẩu (mặc định: 1234)"
+  		echo -e "${BLUE}Enter password (default: 1234)"
     	read passw
       	passw="${passw:=1234}"
 		dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default' 
-        echo -e "${GREEN}Đang tạo user"
+        echo -e "${GREEN}Creating user"
   		# Create user
     	dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username"
       	dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UserShell "/bin/zsh"
@@ -42,7 +42,7 @@ select opt in "${options[@]}"; do
 		echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/OSX/etc/hosts
 		echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/OSX/etc/hosts
 		echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/OSX/etc/hosts
-        echo -e "${GREEN}Chặn host thành công${NC}"
+        echo -e "${GREEN}Host blocking successful${NC}"
 		# echo "Remove config profile"
   	touch /Volumes/Data/private/var/db/.AppleSetupDone
         rm -rf /Volumes/OSX/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
